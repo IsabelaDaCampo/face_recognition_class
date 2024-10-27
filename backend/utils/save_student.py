60cb14b3f7d48e6f05490fec46e71531ce1bb6e5
Isabela Maria <isabeladacampo@gmail.com>
2024-10-27 10:47:15 -0300

    Alterado função de save, recognition e criada função de presença dos alunos.

--- backend/utils/save_student.py
+++ backend/utils/save_student.py
@@ -1,18 +1,26 @@
 import os
 import cv2
 
-def salvar_foto_aluno(image_path, aluno_nome):
+def save_student_picture(image_path, student_name, student_class):
     alunos_dir = 'models/alunos/'
 
-    if not os.path.exists(alunos_dir):
-        os.makedirs(alunos_dir)
+    class_dir = os.path.join(alunos_dir, student_class)
+    if not os.path.exists(class_dir):
+        os.makedirs(class_dir)
 
     img = cv2.imread(image_path)
     
     if img is None:
         return {'status': 'Falha ao carregar a imagem'}
 
-    aluno_foto_path = os.path.join(alunos_dir, f'{aluno_nome}.jpg')
+    aluno_foto_base = f'{student_name}_{student_class}'
+    
+    contador = 1
+    while True:
+        aluno_foto_path = os.path.join(class_dir, f'{aluno_foto_base}_{contador}.jpg')
+        if not os.path.exists(aluno_foto_path):
+            break
+        contador += 1
 
     cv2.imwrite(aluno_foto_path, img)
 
